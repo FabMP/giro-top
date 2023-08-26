afe3dc25c9a5c615b317d54b2da6adb450101e82
FabMP <fabricio05mp@gmail.com>
2023-08-25 23:44:36 -0300

    Definições do banco de dados atualizadas

--- knexconfig.js
+++ knexconfig.js
@@ -1,6 +1,22 @@
 export const knexConfig = {
-    development: {
-      client: 'pg',
-      connection: process.env.POSTGRES_URL+"?sslmode=require"
+  development: {
+    client: 'pg',
+    connection: `${process.env.POSTGRES_URL}?sslmode=require`,
+    migrations: {
+      directory: './migrations'
+    },
+    seeds: {
+      directory: './seeds'
     }
-  };
+  },
+  production: {
+    client: 'pg',
+    connection: `${process.env.POSTGRES_URL}?sslmode=require`,
+    migrations: {
+      directory: './migrations'
+    },
+    seeds: {
+      directory: './seeds'
+    }
+  }
+};
--- knexfile.cjs
+++ knexfile.cjs
@@ -1,6 +1,22 @@
 module.exports = {
   development: {
     client: 'pg',
-    connection: process.env.POSTGRES_URL+"?sslmode=require"
+    connection: `${process.env.POSTGRES_URL}?sslmode=require`,
+    migrations: {
+      directory: './migrations'
+    },
+    seeds: {
+      directory: './seeds'
+    }
+  },
+  production: {
+    client: 'pg',
+    connection: `${process.env.POSTGRES_URL}?sslmode=require`,
+    migrations: {
+      directory: './migrations'
+    },
+    seeds: {
+      directory: './seeds'
+    }
   }
 };
