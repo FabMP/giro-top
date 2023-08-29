78e6a986e0db1b1716bef019688a0a3656170c5e
FabMP <fabricio05mp@gmail.com>
2023-08-29 20:04:29 -0300

    Correção do banco de dados

--- src/routes/login/+page.server.js
+++ src/routes/login/+page.server.js
@@ -1,5 +1,4 @@
 import { redirect } from "@sveltejs/kit";
-import bcrypt from 'bcrypt';
 import { db, sql } from '@vercel/postgres';
 
 const client = () => db.connect({
@@ -14,9 +13,8 @@ export const actions = {
         const passwordForm = data.get('password');
         const adm = await sql`SELECT * FROM administrador WHERE nome = ${nomeForm}`;
         const password = await sql`SELECT password FROM administrador WHERE nome = ${nomeForm}`
-        const senha = bcrypt.compare(passwordForm, password);
 
-        if (adm && senha) {
+        if (adm && password) {
             cookies.set("access", "true", { path: "admin/galerias", SameSite: "strict",  });
             throw redirect(302, 'admin/galerias');
           }
