9100622186f45140c446a3a07b24d0573ff31d9e
FabMP <fabricio05mp@gmail.com>
2023-10-12 21:17:22 -0300

    Correção no login

--- src/routes/login/+page.server.js
+++ src/routes/login/+page.server.js
@@ -1,5 +1,6 @@
 import { redirect } from "@sveltejs/kit";
 import { db, sql } from '@vercel/postgres';
+import bcrypt from 'bcrypt';
 
 const client = () => db.connect({
     connectionString: process.env.POSTGRES_URL
@@ -14,7 +15,7 @@ export const actions = {
         const adm = await sql`SELECT * FROM administrador WHERE nome = ${nomeForm}`;
         const password = await sql`SELECT password FROM administrador WHERE nome = ${nomeForm}`
 
-        const bate = await bcrypt.compare(passwordForm, password);
+        const bate = bcrypt.compare(passwordForm, password);
 
         if (adm && bate) {
             cookies.set("access", "true", { path: "admin/galerias", SameSite: "strict"});
