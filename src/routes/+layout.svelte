226ba295a45dfb6154dfc87c64bdc7a2b7e92ad4
FabMP <fabricio05mp@gmail.com>
2024-03-27 14:48:30 -0300

    Adição de tags do Google

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,8 +4,15 @@
         <img src="/girotop-logo.png" alt="Logo" href="/" id="logo" />
       </a>
     
+      <div id="toggle">
+        <span class="navbar-toggler" type="button" data-bs-toggle="modal" data-bs-target="#exampleModal">
+          <span class="navbar-toggler-icon"></span>
+        </span>
+      </div>
+
     <div class="collapse navbar-collapse d-flex justify-content-end">
-      <ul class="navbar-nav esconde">
+      <div class="esconde">
+      <ul class="navbar-nav">
         <li class="nav-item">
           <a
             class="nav-link active icon-link icon-link-hover"
@@ -19,15 +26,12 @@
           >
         </li>
       </ul>
+      </div>
     </div>
     
-    <div id="toggle">
-      <span class="navbar-toggler" type="button" data-bs-toggle="modal" data-bs-target="#exampleModal">
-        <span class="navbar-toggler-icon"></span>
-      </span>
-    </div>
+    
+  </div>
 </nav>
-
 
 <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog modal-fullscreen">
@@ -37,8 +41,17 @@
         <span type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></span>
       </div>
       <div class="modal-body">
-        <ul class="list-group list-group-flush"> 
-          <a class="orange-texto list-group-item botaomenu"  href="/">Home</a>
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item">
+          <div class="d-flex justify-content-center">
+            <a href="/" class="orange-texto">Home</a>
+          </div>
+          </li>
+          <li class="list-group-item">
+            <div class="d-flex justify-content-center">
+              <a href="/#diferenciais" class="orange-texto">Diferenciais</a>
+            </div>
+            </li>  
           <button class="list-group-item" on:click={whatsapp}><p class="orange-texto">Contato</p></button>
         </ul>
       </div>
